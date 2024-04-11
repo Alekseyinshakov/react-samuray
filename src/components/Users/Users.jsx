cf8b59381d93f84c12174b53cdbf85498488d79c
Alexey <alexeyinshakov992@gmail.com>
2024-04-11 20:04:05 +0300

    51 lesson

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,38 +1,43 @@
-import styles from './Users.module.scss'
+import styles from './Users.module.scss';
+import axios from 'axios';
+import noAvatar from './no-avatar.jpg'
+
 export let Users = (props) => {
 
-    let users = [
-        {
-            id: 1,
-            name: "Dima",
-            status: "status text dffkfdv idfufd sadfdr",
-            avatar: "/img/1.jpd",
-            country: "Belarus",
-            city: "Minsk",
-            followed: true
-        },
-        {
-            id: 2,
-            name: "Sasha",
-            status: "status text",
-            avatar: "/img/1.jpd",
-            country: "Belarus",
-            city: "Minsk",
-            followed: true
-        },
-        {
-            id: 3,
-            name: "Kolya",
-            status: "status text",
-            avatar: "/img/1.jpd",
-            country: "Belarus",
-            city: "Minsk",
-            followed: false
-        }
-    ];
+    // let users = [
+    //     {
+    //         id: 1,
+    //         name: "Dima",
+    //         status: "status text dffkfdv idfufd sadfdr",
+    //         avatar: "/img/1.jpd",
+    //         country: "Belarus",
+    //         city: "Minsk",
+    //         followed: true
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "Sasha",
+    //         status: "status text",
+    //         avatar: "/img/1.jpd",
+    //         country: "Belarus",
+    //         city: "Minsk",
+    //         followed: true
+    //     },
+    //     {
+    //         id: 3,
+    //         name: "Kolya",
+    //         status: "status text",
+    //         avatar: "/img/1.jpd",
+    //         country: "Belarus",
+    //         city: "Minsk",
+    //         followed: false
+    //     }
+    // ];
+
     if (props.users.length === 0) {
-        console.log('if')
-        props.setUsers(users)
+        axios.get('https://social-network.samuraijs.com/api/1.0/users?count=100&page=5').then(responce => {
+            props.setUsers(responce.data.items)
+        })
     }
 
 
@@ -45,16 +50,18 @@ export let Users = (props) => {
                         <div className={styles.user}>
                             <div className={styles.col1}>
                                 <div className={styles.avatar_wrap}>
-                                    <img src="" alt=""/>
+                                    <img src={user.photos.small || noAvatar} alt=""/>
                                 </div>
 
                                 {user.followed ?
                                     <button onClick={() => {
 
-                                        props.unfollow(user.id)}} className={styles.followed}>unfollow</button> :
+                                        props.unfollow(user.id)
+                                    }} className={styles.followed}>unfollow</button> :
                                     <button onClick={() => {
 
-                                        props.follow(user.id)}} className={styles.unfollowed}>follow</button>
+                                        props.follow(user.id)
+                                    }} className={styles.unfollowed}>follow</button>
                                 }
                             </div>
                             <div className={styles.col2}>
