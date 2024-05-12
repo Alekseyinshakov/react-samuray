5ef60a8310fcd9a08f105cf24b286607c7290461
Alexey <alexeyinshakov992@gmail.com>
2024-05-12 12:39:02 +0300

    reselect library

--- src/Redux/users-selectors.js
+++ src/Redux/users-selectors.js
@@ -1,6 +1,12 @@
+import {createSelector} from "reselect";
+
 export const getUsers = (state) => {
-    return state.usersPage.users
+    return state.usersPage.users.filter(u => true)
 }
+
+export const getUsersSuper = createSelector(() => {
+    
+})
 
 export const getTotalUsers = (state) => {
     return state.usersPage.totalUsers
